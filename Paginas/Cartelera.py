ea18b91ad1e5ea2f472df5bfb8cb4534bb8a9ec8
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-11-15 11:27:41 -0300

    Update Cartelera.py

--- Paginas/Cartelera.py
+++ Paginas/Cartelera.py
@@ -5,12 +5,12 @@ def Cartelera():
     st.title("Cartelera de Películas por Género")
 
     # Verificar si ya hay películas seleccionadas por género
-    if "selected_genre_movies" in st.session_state:
-        genre_movies = st.session_state.selected_genre_movies
+    if "selected_genre_movies_genre" in st.session_state:
+        genre_movies = st.session_state.selected_genre_movies_genre
 
         # Mostrar las películas por género
         for idx, row in genre_movies.iterrows():
-            st.image(row['poster_url'], caption=row['name'], width=220)
+            st.image(row['link'], caption=row['name'], width=220)
             st.write(row['description'])
 
         # Aquí puedes agregar la funcionalidad para navegar entre películas, agregar más detalles, etc.
