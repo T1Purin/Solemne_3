238d11a8ced819bf5cd91438300ef7a1f6df10d9
Seba <sebastian141718floresflores@gmail.com>
2024-11-18 00:23:16 -0300

    w

--- Paginas/Main.py
+++ Paginas/Main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from streamlit_echarts import st_echarts
 import random
 
 def main():
@@ -177,6 +178,43 @@ def main():
                     # Cambiar la página a "DPeliculas" (detalles de la película)
                     st.session_state.page = "Cartelera"
 
+    def graficos():
+        
+        # Primer gráfico
+        genres_rating_converg = pd.merge(dg_combined, dm, on='id')
+        # Asegurarse de que la columna 'rating' sea numérica
+        genres_rating_converg['rating'] = pd.to_numeric(genres_rating_converg['rating'], errors='coerce')
+
+        # Eliminar filas con valores NaN en 'rating'
+        genres_rating_converg = genres_rating_converg.dropna(subset=['rating'])
+        genres_prom = genres_rating_converg.sort_values(by='rating', ascending=False)
+
+        # Genera las listas para los géneros y ratings
+        genres = genres_prom['genre'].tolist()
+        rating = genres_prom['rating'].tolist()
+
+        # Configura tus opciones de gráfico ECharts
+        gpr = {
+            "title": {
+                "text": "Promedio de rating por géneros"
+            },
+            "tooltip": {},
+            "xAxis": {
+                "type": "category",  # Especifica que es un gráfico de categorías
+                "data": genres  # Asignar directamente la lista de géneros
+            },
+            "yAxis": {}
+            ,
+            "series": [{
+                "name": "Rating",  # Nombre de la serie
+                "type": "bar",  # Tipo de gráfico (línea)
+                "data": rating  # Los datos para la serie
+            }]
+        }
+
+        # Renderizar el gráfico
+        st_echarts(options=gpr)
+
     # Verificar cuál opción fue elegida y mostrar el buscador correspondiente
     if 'search_type' not in st.session_state:
         st.session_state.search_type = 'movie'  # Valor por defecto (buscar por película)
@@ -304,3 +342,5 @@ def main():
         st.write("---")
 
     st.subheader('Estadisticas:')
+
+    graficos()
