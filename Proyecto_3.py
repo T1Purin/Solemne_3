cb2c9833a89a4fbf3f647f8effaa0a259b9c752b
Constanza Caruz <coni@MacBook-Air-de-Constanza.local>
2024-11-22 13:35:00 -0300

    w

--- Proyecto_3.py
+++ Proyecto_3.py
@@ -3,10 +3,11 @@ from Paginas.Main import main
 from Paginas.Pagina_principal import principal
 from Paginas.Cartelera import cartelera
 
+if 'page' not in st.session_state:
+    st.session_state.page = "Main"
+
 #Gestionar la navegación entre páginas
 def app():
-    if 'page' not in st.session_state:
-        st.session_state.page = "Main"
 
     if st.session_state.page == "Main":
         main()
