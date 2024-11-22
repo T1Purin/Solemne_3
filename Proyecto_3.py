66db5db0c23e830deeada8f822976d133063c8f6
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-11-22 13:47:54 -0300

    Actualizar Proyecto_3.py

--- Proyecto_3.py
+++ Proyecto_3.py
@@ -3,8 +3,7 @@ from Paginas.Main import main
 from Paginas.Pagina_principal import principal
 from Paginas.Cartelera import cartelera
 
-if 'page' not in st.session_state:
-    st.session_state.page = "Main"
+st.session_state.page = "Main"
 
 #Gestionar la navegación entre páginas
 def app():
