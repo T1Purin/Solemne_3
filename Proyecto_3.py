159d95dfcb82c4bde7d7cef087e5f4f362e44a6a
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-11-24 21:20:19 -0300

    Update Proyecto_3.py

--- Proyecto_3.py
+++ Proyecto_3.py
@@ -9,10 +9,13 @@ st.session_state.page = "Main"
 def app():
 
     if st.session_state.page == "Main":
+        st.set_page_config(layout="wide")
         main()
     elif st.session_state.page == "DPeliculas":
+        st.set_page_config(layout="wide")
         principal()
     elif st.session_state.page == "Cartelera":
+        st.set_page_config(layout="wide")
         cartelera()
 
 app()
